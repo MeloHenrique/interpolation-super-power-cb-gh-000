c847f1f7c7b69f56a68f5597cd5905f14c491551
Henrique Melo <henriquemelo@protonmail.com>
2019-08-28 21:52:43 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 # Write your #display_rainbow method here
-colors = ['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def display_rainbow (colors)
+def display_rainbow (cona)
 
-  puts "colors"
+  puts "R: #{cona[0]}, O: #{cona[1]}, Y: #{cona[2]}, G: #{cona[3]}, B: #{cona[4]}, I: #{cona[5]}, V: #{cona[6]} "
 
 end
 
-display_rainbow(colors)
+display_rainbow
